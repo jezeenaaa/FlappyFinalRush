0ce289abe0531dfbbad2459a8159e979d73cca73
jezeenaaa <724214@student.dusd.net>
2022-12-16 00:04:05 -0800

    Added obstacles for the bird and made a inifinite background
    i changed the way the bird looks and i added obstacles for the bird also the bird could keep flapping infinitely.

--- FlappyBirdFinalRush/Assets/Scripts/RepeatingBackground.cs
+++ FlappyBirdFinalRush/Assets/Scripts/RepeatingBackground.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RepeatingBackGround : MonoBehaviour
+public class RepeatingBackground : MonoBehaviour
 {
+    private BoxCollider2D groundCollider;
+    private float groundHorizontalLength;
+
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        groundCollider = GetComponent<BoxCollider2D>();
+        groundHorizontalLength = groundCollider.size.x;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x < -groundHorizontalLength)
+        {
+            RepositionBackground();
+
+        }
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2 (groundHorizontalLength * 2f, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
     }
 }
+
